ce6dedee7e61dd2ce91cd3d89af1cd18ba524680
letsgetwork <asharirhmn@gmail.com>
2024-02-06 23:37:44 +0800

    ..

--- dist/redirect.js
+++
@@ -1,7 +0,0 @@
-// Check if the current URL is not the target URL
-if (window.location.href !== "https://ashrtv.my.id/") {
-  // Redirect after 1.5 seconds
-  setTimeout(function() {
-    window.location.href = "https://ashrtv.my.id/";
-  }, 1500);
-}
