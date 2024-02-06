4c907bad5757727a9c2de91aa9d7f96be68968c6
letsgetwork <asharirhmn@gmail.com>
2024-02-07 01:22:41 +0800

    adding redirect.js and edit index html as well

---
+++ dist/redirect.js
@@ -0,0 +1,7 @@
+// Check if the current URL is not the target URL
+if (window.location.href !== "https://ashrtv.my.id/") {
+  // Redirect after 1.5 seconds
+  setTimeout(function() {
+    window.location.href = "https://ashrtv.my.id/";
+  }, 1500);
+}
